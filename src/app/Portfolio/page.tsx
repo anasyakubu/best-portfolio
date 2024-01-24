be8c372e54a9561a7c16b4bb5bd90bba2d6bcee9
anasyakubu <yakubuanas04@gmail.com>
2024-01-24 23:13:50 +0100

    DisplayPortfolio Section Setup

--- src/app/Portfolio/page.tsx
+++ src/app/Portfolio/page.tsx
@@ -1,10 +1,12 @@
 import PortfolioHeader from "@/app/sections/PortfolioHeader/PortfolioHeader";
 import React from "react";
+import DisplayPortfolio from "../sections/DisplayPortfolio/DisplayPortfolio";
 
 const page = () => {
   return (
     <div>
       <PortfolioHeader />
+      <DisplayPortfolio />
     </div>
   );
 };
--- src/app/sections/PortfolioHeader/PortfolioHeader.tsx
+++ src/app/sections/PortfolioHeader/PortfolioHeader.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { FaSquareXTwitter } from "react-icons/fa6";
 import "./PortfolioHeader.scss";
 import Image from "next/image";
-import BannerOne from "../../../../public/Code typing-bro.svg";
+import BannerOne from "../../../../public/Programming-pana.svg";
 import Nav from "@/app/components/Nav/Nav";
 import Link from "next/link";
 // import PrimaryBtn from "@/app/components/PrimaryBtn";
@@ -26,32 +26,15 @@ const PortfolioHeader = () => {
                 className="text-md font-extrabold tracking-wide shadow-2 sm:text-3xl"
                 style={{ color: "#000" }} //#122231
               >
-                Yellow 🫡
+                Work Gallery 📷
               </h1>
               <p
                 className="mt-4 text-sm leading-8"
                 style={{ color: "#122231" }} //#3d4f60 122231
               >
-                My name is Anas. I`m a{" "}
-                <span className="text-blue-600">Software Engineer</span> and a
-                IT student at
-                <Link
-                  href="https://www.buk.edu.ng/"
-                  target="_blank"
-                  className="text-blue-600"
-                >
-                  {" "}
-                  Bayero University Kano
-                </Link>{" "}
-                .
-                <br /> My passion for software engineering emerged as the world
-                turned digital and I know find myself creating{" "}
-                <span className="text-blue-600">solutions</span> to the everyday
-                problems that frame our human experience. <br />{" "}
-                <span className="text-blue-600">Programming</span> for
-                inclusively, equitability, and universal accessibility are my
-                core values when it comes to coding positive experiences with
-                people in mind.
+                Take a look at some of the{" "}
+                <span className="text-blue-600">works</span> I have done so far,
+                for the past Years
               </p>
             </div>
           </div>
